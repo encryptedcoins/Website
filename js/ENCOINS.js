36b7ad8636a500df7c29dc220a1fe550ee9e7469
ENCOINS <111982001+encryptedcoins@users.noreply.github.com>
2023-06-19 14:34:32 +0200

    Fix DAO link

--- js/ENCOINS.js
+++ js/ENCOINS.js
@@ -358,3 +358,100 @@ async function addrLoad(addrInput)
     return;
   }
 }
+
+//////////////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////// DAO functions //////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////////////////
+
+function toUTF8Array(str) {
+  var utf8 = [];
+  for (var i=0; i < str.length; i++) {
+      var charcode = str.charCodeAt(i);
+      if (charcode < 0x80) utf8.push(charcode);
+      else if (charcode < 0x800) {
+          utf8.push(0xc0 | (charcode >> 6), 
+                    0x80 | (charcode & 0x3f));
+      }
+      else if (charcode < 0xd800 || charcode >= 0xe000) {
+          utf8.push(0xe0 | (charcode >> 12), 
+                    0x80 | ((charcode>>6) & 0x3f), 
+                    0x80 | (charcode & 0x3f));
+      }
+      // surrogate pair
+      else {
+          i++;
+          // UTF-16 encodes 0x10000-0x10FFFF by
+          // subtracting 0x10000 and splitting the
+          // 20 bits of 0x0-0xFFFFF into two halves
+          charcode = 0x10000 + (((charcode & 0x3ff)<<10)
+                    | (str.charCodeAt(i) & 0x3ff));
+          utf8.push(0xf0 | (charcode >>18), 
+                    0x80 | ((charcode>>12) & 0x3f), 
+                    0x80 | ((charcode>>6) & 0x3f), 
+                    0x80 | (charcode & 0x3f));
+      }
+  }
+  return utf8;
+}
+
+async function daoPollVoteTx(n, walletName, answer)
+{
+  // loading CardanoWasm
+  await loader.load();
+  const CardanoWasm = loader.Cardano;
+
+  await lucidLoader.load();
+  const lucid = await lucidLoader.Lucid.new(
+    new lucidLoader.Blockfrost("https://cardano-mainnet.blockfrost.io/api/v0", "mainnetK4sRBCTDwqzK1KRuFxnpuxPbKF4ZQrnl"),
+    "Mainnet",
+  )
+
+  try {
+    //loading wallet
+    const api = await walletAPI(walletName);
+    lucid.selectWallet(api);
+
+    const changeAddress    = CardanoWasm.Address.from_bytes(fromHexString(await api.getChangeAddress()));
+    const baseAddress      = CardanoWasm.BaseAddress.from_address(changeAddress);
+    const stakeKeyHashCred = baseAddress.stake_cred();
+    const stakeKeyHash     = stakeKeyHashCred.to_keyhash();
+    
+    const plc_lst = CardanoWasm.PlutusList.new();
+    const tag1 = CardanoWasm.PlutusData.new_bytes(toUTF8Array("ENCOINS"));
+    const tag2 = CardanoWasm.PlutusData.new_bytes(toUTF8Array("Poll #" + n));
+    const tag3 = CardanoWasm.PlutusData.new_bytes(stakeKeyHash.to_bytes());
+    const tag4 = CardanoWasm.PlutusData.new_bytes(toUTF8Array(answer));
+    plc_lst.add(tag1);
+    plc_lst.add(tag2);
+    plc_lst.add(tag3);
+    plc_lst.add(tag4);
+    const plc_msg = CardanoWasm.PlutusData.new_list(plc_lst);
+
+    const tx = await lucid.newTx()
+      .addSignerKey(toHexString(stakeKeyHash.to_bytes()))
+      .payToAddressWithData(changeAddress.to_bech32(), { inline: toHexString(plc_msg.to_bytes()) }, { lovelace: 1500000n })
+      .complete();
+
+    const signedTx = await tx.sign().complete();
+
+    const txHash = await signedTx.submit();
+    console.log(txHash);
+
+    setInputValue("elementPoll" + n, "Thank you for voting! Come back later to see the results.");
+
+    changeAddress.free();
+    baseAddress.free();
+    stakeKeyHashCred.free();
+    stakeKeyHash.free();
+
+    plc_lst.free();
+    tag1.free();
+    tag2.free();
+    tag3.free();
+    tag4.free();
+    plc_msg.free();
+  } catch (e) {
+    console.log("Error: " + e.message);
+    return;
+  }
+};
